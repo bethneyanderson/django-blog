908e6d33ff86231b17d6951c0aec0eb857951957
bethneyanderson <bethneyanderson95@gmail.com>
2025-07-14 16:39:54 +0100

    Fix SECRET_KEY configuration with fallback value

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qe(&c$&873d##(lv7(yb&ql3r*5v6mpu)dk5-ucl+%g$&es+9t'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-qe(&c$&873d##(lv7(yb&ql3r*5v6mpu)dk5-ucl+%g$&es+9t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
